15c719753b5ea5a80e7f43383dcd6c4ecdfff9c7
MaxikingL <maximuslema@gmail.com>
2024-02-06 12:41:17 +0100

    Initiala commit

---
+++ src/test/java/com/TauronDigitalMeter/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.TauronDigitalMeter.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
